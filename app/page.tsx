b312069b3a9bdb2c8c7cda9c8842fc3099dd497a
ShiromaYasuyuki <ShiromaYasuyuki11@gmail.com>
2024-08-28 07:53:31 +0000

    検索フォーム途中

--- app/page.tsx
+++ app/page.tsx
@@ -52,37 +52,48 @@ export default function GourmetsSearch() {
           <Button type="submit">検索</Button>
         </div>
         <div className="flex items-center space-x-4">
-          <Input
-            type="text"
-            name="people"
-            placeholder="人数"
-            className="max-w-xs w-20"
-          />
-          <select
-            name="budget"
-            className="max-w-xs w-20 border border-gray-300 rounded-md p-2"
-          >
-            <option value="B009">~500円</option>
-            <option value="B010">501～1000円</option>
-            <option value="B011">1001～1500円</option>
-            <option value="B001">1501～2000円</option>
-            <option value="B002">2001～3000円</option>
-            <option value="B003">3001～4000円</option>
-            <option value="B008">4001～5000円</option>
-            <option value="B004">5001～7000円</option>
-            <option value="B005">7001～10000円</option>
-            <option value="B006">10001～15000円</option>
-            <option value="B012">15001～20000円</option>
-            <option value="B013">20001～30000円</option>
-            <option value="B014">30001円～</option>
+          <label className="flex items-center space-x-2">
+          <span>人数:</span>
+            <Input
+              type="text"
+              name="people"
+              placeholder="人数"
+              defaultValue="2"
+              className="max-w-xs w-10"
+            />
+          </label>
+          <label className="flex items-center space-x-2">
+            <span>予算:</span>
+            <select
+              name="budget"
+              className="max-w-xs w-30 border border-gray-300 rounded-md p-2 text-xs"
+            >
+              <option value="B009">~500円</option>
+              <option value="B010">501～1000円</option>
+              <option value="B011">1001～1500円</option>
+              <option value="B001">1501～2000円</option>
+              <option value="B002">2001～3000円</option>
+              <option value="B003">3001～4000円</option>
+              <option value="B008">4001～5000円</option>
+              <option value="B004">5001～7000円</option>
+              <option value="B005">7001～10000円</option>
+              <option value="B006">10001～15000円</option>
+              <option value="B012">15001～20000円</option>
+              <option value="B013">20001～30000円</option>
+              <option value="B014">30001円～</option>
 
-          </select>
-          <Input
-            type="text"
-            name="location"
-            placeholder="場所"
-            className="max-w-xs w-20"
-          />
+            </select>
+          </label>
+          <label className="flex items-center space-x-2">
+            <span>場所:</span>
+            <Input
+              type="text"
+              name="location"
+              placeholder="場所"
+              defaultValue="新都心"
+              className="max-w-xs w-20"
+            />
+          </label>
         </div>
       </form>
     </div>
