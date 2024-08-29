d8a39d2534924fc67c79de6190ea45d38239d478
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2024-08-29 04:44:08 +0000

    フッターにライセンス情報表示

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,14 @@
- 
 export default function Footer() {
-    return (
-      <footer className="bg-background border-t py-4 mt-8">
-        <div className="container mx-auto px-4">
-          <p className="text-center text-sm text-muted-foreground">
-            {new Date().getFullYear()} Search Gourmet. 
-          </p>
-        </div>
-      </footer>
-    )
-  }
-   
+  return (
+    <footer className="bg-background border-t py-4 mt-8">
+      <div className="container mx-auto px-4">
+        <p className="text-center text-sm text-muted-foreground">
+          {new Date().getFullYear()} Search Gourmet. Powered by{" "}
+          <a href="http://webservice.recruit.co.jp/">
+            ホットペッパーグルメ Webサービス
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
