4b2d3f82b5371ac8e88fa617b820d4b2fabd3589
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2024-08-29 04:43:15 +0000

    ヘッダー、サイト名にリンクを追加

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 import { LoginButton, LogoutButton } from "@/app/components/buttons";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function Header() {
   const { data: session } = useSession();
@@ -9,7 +10,9 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 bg-background border-b">
       <nav className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <h1 className="text-xl font-bold">レビューナビ</h1>
+        <h1 className="text-xl font-bold">
+          <Link href="/">レビューナビ </Link>
+        </h1>
         <div className="flex items-center space-x-4">
           {!user ? <LoginButton /> : <LogoutButton />}
           <ThemeToggle />
